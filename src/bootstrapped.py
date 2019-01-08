d38ba6afda4ee9db67b01dd61c7331ae7fe60251
Florin Gogianu <florin.gogianu@gmail.com>
2019-01-08 12:16:06 +0200

    Implement a Deterministic Bootstrapp Policy.

--- src/bootstrapped.py
+++ src/bootstrapped.py
@@ -2,15 +2,111 @@
     estimators.
 """
 from functools import partial
+from typing import NamedTuple
+
 import torch
+from gym.spaces import Discrete
+
 from wintermute.policy_improvement import DQNPolicyImprovement, get_dqn_loss
+from wintermute.policy_evaluation.exploration_schedules import get_schedule
 
 
-class BootstrappedEpsilonGreedyPolicy:
-    """ TODO: implement this.
+class DeterministicEnsembleOutput(NamedTuple):
+    """ The output of the deterministic policy. """
+
+    action: int
+    q_value: float
+    sigma: object
+    full: object
+    posterior: object
+    vote_cnt: object
+
+
+class BootstrappedPolicy:
+    """ Implements the policy evaluation step for bootstrapped estimators
+        (ensembles). It has two behaviours:
+
+        1. Approximates Thompson Sampling by picking one ensemble component
+        at the begining of an episode and evaluating the policy. For this it
+        provides the `sample_posterior_idx` method.
+
+        2. The entire ensemble is used for taking actions. This behaviour
+        happens on `is_thompson=False` and it has two modes:
+            i) The action is the argmax of the Q-values obtained by averaging
+            over the ensemble.
+            ii) The action is the one picked by the majority of the ensemble
+            components (`vote`==True).
     """
-    pass
 
+    def __init__(self, estimator, is_thompson=False, vote=True):
+        self.__estimator = estimator
+        self.__posterior_idx = None
+        self.__vote = vote
+        self.__ensemble_sz = len(self.__estimator)
+
+        # TODO: check this for batch inputs also
+        if is_thompson:
+            self.get_action = self.__thompson_action
+        elif vote:
+            self.get_action = self.__voting_action
+        else:
+            self.get_action = self.__mean_action
+
+    def __thompson_action(self, state):
+        assert (
+            self.__posterior_idx is not None
+        ), "Call `sample_posterior_idx` first."
+
+        ensemble_qvals = self.__estimator(state).squeeze()
+        qvals = ensemble_qvals[self.__posterior_idx]
+        qval, argmax_a = qvals.max(0)
+
+        return DeterministicEnsembleOutput(
+            action=argmax_a.item(),
+            sigma=ensemble_qvals.var(0)[argmax_a].item(),
+            q_value=qval.item(),
+            full=ensemble_qvals,
+            posterior=self.__posterior_idx,
+            vote_cnt=False,
+        )
+
+    def __mean_action(self, state):
+        ensemble_qvals = self.__estimator(state).squeeze()
+        qvals = ensemble_qvals.mean(0)
+        qval, argmax_a = qvals.max(0)
+
+        return DeterministicEnsembleOutput(
+            action=argmax_a.item(),
+            sigma=ensemble_qvals.var(0)[argmax_a].item(),
+            q_value=qval.item(),
+            full=ensemble_qvals,
+            posterior="mean",
+            vote_cnt=False,
+        )
+
+    def __voting_action(self, state):
+        ensemble_qvals = self.__estimator(state).squeeze()
+        act_no = ensemble_qvals.shape[1]
+
+        qvals, argmaxes = ensemble_qvals.max(1)
+        votes = [(argmaxes == act).sum(0) for act in range(act_no)]
+        vote_cnt, argmax_a = torch.stack(votes, 0).max(0)
+
+        return DeterministicEnsembleOutput(
+            action=argmax_a.item(),
+            sigma=ensemble_qvals.var(0)[argmax_a].item(),
+            q_value=qvals[argmaxes == argmax_a].mean(),
+            full=ensemble_qvals,
+            posterior="vote",
+            vote_cnt=vote_cnt,
+        )
+
+    def sample_posterior_idx(self):
+        """ Samples one of the components of the ensemble and returns its
+            index.
+        """
+        self.__posterior_idx = torch.randint(0, self.__ensemble_sz, (1,)).item()
+        return self.__posterior_idx
 
 
 class BootstrappedDQNPolicyImprovement(DQNPolicyImprovement):
@@ -120,11 +216,32 @@ class BootstrappedDQNPolicyImprovement(DQNPolicyImprovement):
         loss.backward()
         self.update_estimator()
 
-    @posterior_idx.setter
-    def posterior_idx(self, idx):
+    def set_posterior_idx(self, idx):
         assert self.__is_thompson, (
             "Calling this setter means you want to train the ensemble in a "
             + "Thompson sampling setup but you didn't set `is_thompson` in the "
             + "constructor."
         )
         self.__posterior_idx = idx
+
+
+if __name__ == "__main__":
+    from wintermute.estimators import AtariNet, BootstrappedAtariNet
+
+    net = AtariNet(1, 4, 3)
+    ens = BootstrappedAtariNet(net, 7)
+
+    x = torch.randint(0, 255, (1, 4, 84, 84), dtype=torch.uint8)
+
+    policies = [
+        ("thompson", BootstrappedPolicy(ens, is_thompson=True, vote=False)),
+        ("mean    ", BootstrappedPolicy(ens, is_thompson=False, vote=False)),
+        ("vote    ", BootstrappedPolicy(ens, is_thompson=False, vote=True)),
+    ]
+
+    policies[0][1].sample_posterior_idx()
+
+    for name, policy in policies:
+        with torch.no_grad():
+            print("\n", name, " --- ")
+            print(policy.get_action(x))
